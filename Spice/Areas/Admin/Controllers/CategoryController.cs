041a95923dc9fd4b5bb88d7d00827cf795edee9d
Le Anh Nhan <lenhanasus4@gmail.com>
2020-04-20 13:53:32 +0700

    add controller category

---
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Common;
+using Spice.Data;
+using System.Threading.Tasks;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area(Constant.Area_Admin)]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _db.Category.ToListAsync());
+        }
+    }
+}
---
+++ Spice/Common/Constant/View.cs
@@ -0,0 +1,10 @@
+﻿namespace Spice.Common
+{
+    public partial class Constant
+    {
+        /// <summary>
+        /// ~/Views/Shared/_Layout.cshtml
+        /// </summary>
+        public const string View_Layout = "~/Views/Shared/_Layout.cshtml";
+    }
+}
