565c6722637cc944fcef21d74f0f4c75a21acf23
Le Anh Nhan <lenhanasus4@gmail.com>
2020-05-01 13:23:44 +0700

    complete coupon add attribute disabled for input, select on views category, subcategory, menuitem and coupon

--- Spice/Areas/Admin/Controllers/CouponController.cs
+++ Spice/Areas/Admin/Controllers/CouponController.cs
@@ -76,12 +76,102 @@ namespace Spice.Areas.Admin.Controllers
             return View(coupon);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var coupon = await _db.Coupon.FindAsync(id);
+            if(coupon == null)
+            {
+                return NotFound();
+            }
+            return View(coupon);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Coupon coupon)
+        {
+            if (ModelState.IsValid)
+            {
+                // get picture
+                var files = HttpContext.Request.Form.Files;
+                if (files.Count > 0)
+                {
+                    byte[] picture = null;
+                    using (var fileStream = files[0].OpenReadStream())
+                    {
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            await fileStream.CopyToAsync(memoryStream);
+                            picture = memoryStream.ToArray();
+                        }
+                    }
+                    coupon.Picture = picture;
+                }
+                _db.Coupon.Update(coupon);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(coupon);
+        }
+
         #endregion
 
         #region Details
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var coupon = await _db.Coupon.FindAsync(id);
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+            return View(coupon);
+        }
+
+
         #endregion
 
         #region Delete
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var coupon = await _db.Coupon.FindAsync(id);
+            if (coupon == null)
+            {
+                return NotFound();
+            }
+            return View(coupon);
+        }
+
+        [HttpPost, ActionName(Constant.Action_Delete)]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var coupon = await _db.Coupon.FindAsync(id);
+            if(coupon == null)
+            {
+                return NotFound();
+            }
+            _db.Coupon.Remove(coupon);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
         #endregion
 
         #endregion
